7999c3228e3e582692948162033142892f7de45f
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-05-13 13:02:29 -0400

    Setup model for landing page

--- app/page/index/route.js
+++ app/page/index/route.js
@@ -3,29 +3,29 @@ import Route from '../../basic/route';
 
 export default Route.extend({
   model() {
-    return [
-      {
-        id: 0,
-        title: 'The Mutants Are Revolting',
-        downloadUrl: 'http://www.example.com',
-        publicUrl: 'http://www.example.com',
-        imageUrl: 'http://www.placecage.com/c/300/350'
-      },
-      {
-        id: 1,
-        title: 'A Taste of Freedom',
-        downloadUrl: 'http://www.example.com',
-        publicUrl: 'http://www.example.com',
-        imageUrl: 'http://www.placecage.com/g/300/350'
-      },
-      {
-        id: 2,
-        title: 'How Hermes Requisitioned His Groove Back',
-        downloadUrl: 'http://www.example.com',
-        publicUrl: 'http://www.example.com',
-        imageUrl: 'http://www.placecage.com/300/350'
-      }
-    ];
+    const fakePage1 = this.store.createRecord('page', {
+      templateId: 0,
+      title: 'The Mutants Are Revolting',
+      downloadUrl: 'http://www.example.com',
+      publicUrl: 'http://www.example.com',
+      imageUrl: 'http://www.placecage.com/c/300/350'
+    });
+    const fakePage2 = this.store.createRecord('page', {
+      templateId: 1,
+      title: 'A Taste of Freedom',
+      downloadUrl: 'http://www.example.com',
+      publicUrl: 'http://www.example.com',
+      imageUrl: 'http://www.placecage.com/g/300/350'
+    });
+    const fakePage3 = this.store.createRecord('page', {
+      templateId: 0,
+      title: 'How Hermes Requisitioned His Groove Back',
+      downloadUrl: 'http://www.example.com',
+      publicUrl: 'http://www.example.com',
+      imageUrl: 'http://www.placecage.com/300/350'
+    });
+
+    return [ fakePage1, fakePage2, fakePage3 ];
   },
 
   actions: {
