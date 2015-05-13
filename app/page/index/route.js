77b6af83a26cfb0bbd46705cd8a641b29e67d794
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-05-13 12:56:18 -0400

    Tweaked card height.

--- app/page/index/route.js
+++ app/page/index/route.js
@@ -9,21 +9,21 @@ export default Route.extend({
         title: 'The Mutants Are Revolting',
         downloadUrl: 'http://www.example.com',
         publicUrl: 'http://www.example.com',
-        imageUrl: 'http://www.placecage.com/c/300/400'
+        imageUrl: 'http://www.placecage.com/c/300/350'
       },
       {
         id: 1,
         title: 'A Taste of Freedom',
         downloadUrl: 'http://www.example.com',
         publicUrl: 'http://www.example.com',
-        imageUrl: 'http://www.placecage.com/g/300/400'
+        imageUrl: 'http://www.placecage.com/g/300/350'
       },
       {
         id: 2,
         title: 'How Hermes Requisitioned His Groove Back',
         downloadUrl: 'http://www.example.com',
         publicUrl: 'http://www.example.com',
-        imageUrl: 'http://www.placecage.com/300/400'
+        imageUrl: 'http://www.placecage.com/300/350'
       }
     ];
   },
