231aa3a4295cf018d5c16dfae09fcb0bab12824b
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-05-19 11:10:39 -0400

    Updated placeholder image height.

--- server/mocks/pages.js
+++ server/mocks/pages.js
@@ -7,19 +7,19 @@ module.exports = function(app) {
       id: '82d01832-d2e0-4043-9017-dd54742c20fe',
       templateId: 0,
       title: 'The Mutants Are Revolting',
-      imageUrl: 'http://www.placecage.com/c/300/350'
+      imageUrl: 'http://www.placecage.com/c/300/450'
     },
     {
       id: '6bd522d8-bb60-4da5-ba29-31d5e5d9b2fd',
       templateId: 1,
       title: 'A Taste of Freedom',
-      imageUrl: 'http://www.placecage.com/g/300/350'
+      imageUrl: 'http://www.placecage.com/g/300/450'
     },
     {
       id: '2ac15ee1-4df5-4812-a796-8c56f28d46eb',
       templateId: 0,
       title: 'How Hermes Requisitioned His Groove Back',
-      imageUrl: 'http://www.placecage.com/300/350'
+      imageUrl: 'http://www.placecage.com/300/450'
     }
   ];
 
