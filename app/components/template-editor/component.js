8023f964bbd9aebfeafca79b0f5cd1bea3de6148
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-05-21 17:05:03 -0400

    First pass at rendering cards into their slots in template editor.

--- app/components/template-editor/component.js
+++ app/components/template-editor/component.js
@@ -1,20 +1,38 @@
 import Ember from 'ember';
 
 const get = Ember.get;
+const set = Ember.set;
 const {
+  EnumerableUtils,
   Component,
-  computed
+  computed,
+  on
 } = Ember;
+
+const {
+  forEach
+} = EnumerableUtils;
 
 export default Component.extend({
   classNames: [ 'templateEdit' ],
   classNameBindings: [ 'isSelectingSlot:is-selecting-slot' ],
+
   layoutName: computed('templateId', {
     get() {
       const templateId = get(this, 'templateId');
 
       return `page-template-${templateId}`;
     }
+  }),
+
+  mapSlots: on('didReceiveAttrs', function() {
+    const cards = get(this, 'page.cards');
+
+    forEach(cards, (card) => {
+      const position = get(card, 'position');
+      const slotPosition = `slot${position}`;
+      set(this, slotPosition, card);
+    });
   }),
 
   actions: {
